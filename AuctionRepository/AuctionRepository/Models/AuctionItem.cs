f8fa02e5d68460e74e988a5517dbcedc87bfc9a4
Fredrik Wängelin <f.wangelin@gmail.com>
2016-11-28 13:34:35 +0100

    Grunden i Projektarbetet

---
+++ AuctionRepository/AuctionRepository/Models/AuctionItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionRepository.Models
+{
+    public enum Category { Kitchen = 1, LivingRoom, BathRoom, BedRoom}
+    class AuctionItem
+    {
+        public double Price { get; set; }
+        public Category Category { get; set; }
+        public string Name { get; set; }
+        public bool isUsed { get; set; }
+        
+    }
+}
