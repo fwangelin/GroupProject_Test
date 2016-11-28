34379b7f9fc3fde5f044e50b2f9ee1e0201032e0
DanielCarlson <info@danielcarlson.net>
2016-11-28 13:43:56 +0100

    Runtime skapad

--- AuctionRepository/AuctionRepository/Program.cs
+++ AuctionRepository/AuctionRepository/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AuctionRepository.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,12 @@ namespace AuctionRepository
     {
         static void Main(string[] args)
         {
+            Runtime runtime = new Runtime();
+            runtime.Start();
+            Person person = new Person();
+
+
+            
         }
     }
 }
---
+++ AuctionRepository/AuctionRepository/Runtime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AuctionRepository
+{
+    public class Runtime
+    {
+        public void Start()
+        {
+            //TODO: Starta programmet meny mfl
+            Console.WriteLine("Startar programmet");
+
+            Console.ReadLine();
+                
+        }
+    }
+}
